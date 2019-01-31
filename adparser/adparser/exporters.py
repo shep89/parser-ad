00e77054485e2503262e0a535adb50dcbd7c4c65
AlekseyS <i.am.shep@gmail.com>
2019-01-31 21:29:02 +0300

    requirements.txt

--- adparser/adparser/exporters.py
+++ adparser/adparser/exporters.py
@@ -1,6 +1,7 @@
 from scrapy.conf import settings
 from scrapy.exporters import CsvItemExporter
 
+
 class TsvItemExporter(CsvItemExporter):
     def __init__(self, *args, **kwargs):
         kwargs['delimiter'] = '\t'
